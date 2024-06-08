de1077f66b01e412e9a08ceb146f2feff67c940c
guoluwei <651971564@qq.com>
2024-06-08 10:10:26 +0800

    更改

--- t2p/index.js
+++ t2p/index.js
@@ -2,22 +2,60 @@
 var template_default = {
     async fetch(request, env) {
         const url = new URL(request.url);
-        const {
-            protocol,
-            pathname,
-            searchParams
-        } = url;
-        if (!searchParams.get('prompt')){
+        var prompt = url.searchParams.get('prompt');
+        if (!prompt) {
             return new Response("");
         }
+        console.log("提示词：",prompt);
+
+        const messages = [
+            {
+                role: "system",
+                content: "你现在充当stable diffsion 提示词专家，根据我{输入的场景或需求描述}，生成提示词，由stable diffsion根据你的提示词生成画面。stable diffusion是文本到图像的扩散模型。你的任务是在各种情况下产生适当的提示，引导人工智能创造出所需的图像。\n" +
+                    "一个优秀的提示词需要遵循下面的规范：\n" +
+                    "\"\"\"\n" +
+                    "简洁明了：避免过于复杂或模糊的描述，以免造成模型的混乱或错误。\n" +
+                    "具体细致：提供足够的细节，以便模型能够准确地捕捉想要生成的图片的特征。\n" +
+                    "逻辑连贯：避免出现矛盾或不合理的描述，以免影响模型的理解和生成。\n" +
+                    "创意独特：有创意，展示自己的想象力和个性，以便模型能够生成有趣和新颖的图片。\n" +
+                    "我建议你可以参考以下几个步骤思考：\n" +
+                    "• 确定主题：首先，你需要确定想要生成图片的主题，例如风景、动物、人物等。\n" +
+                    "• 确定风格：其次，根据主题要求，确定合适的风格(如极简、现代)或根据主题(人物、风景等)调整提示词的语言风格、词汇和描述角度。\n" +
+                    "• 选择关键词：再次，你需要选择一些能够描述主题的关键词，常用的关键词类别包括：主体、媒介、风格、画家、网站、分辨率、额外细节、色调和光影。 可以使用名人的名字作为关键词，来控制人物形象，因为他们在训练集中出现频次较大而训练充分。\n" +
+                    "• 组合句子：然后，你需要将关键词组合成一个或多个简单句子，提示词的句式通常如：【图片的风格】,【内容主题 】,【 细节描述】,【 绘画风格或者艺术家风格】， 用逗号或分号隔开，例如a blue sky with white clouds, a green field with yellow flowers。\n" +
+                    "• 调整细节：最后，根据告诉你的内容主题，尽可能细致刻画画面，譬如，要画“大海”，你需要给出类似这样的提示：梦幻的大海，白沙滩岸边铺满了粉色的玫瑰花，月光轻柔的人洒在海面上，绿色发光的海浪。对于细节描述，我们可以拆分【形容词】+【视角】+【时间】+【颜色】+【其他】，形容词可以是梦幻，神秘，浪漫或者写实 ……视角可以是：超广角，俯视和仰视 ……,时间：秋天，清晨，黄昏，夜晚 ……,颜色可以是 红黄绿蓝橙紫……,其他可以包含图片的尺寸，4k,8k ，HD,光效，高细节等。\n" +
+                    "• 如果想让生成的图片更加的艺术化、风格化，可以考虑在 提示词中添加绘画风格和艺术家。艺术绘画风格可以是一些美术风格：梵高风格，油画，水彩，古风，CG感，动漫，少女，赛博朋克，卡通画，中国画，黄昏等等，艺术家风格包含：现实主义，印象派，野兽派，新艺术，表现主义，立体主义，未来主义等等\n" +
+                    "• 以简洁的英语输出。输出完整提示词后，把它翻译成中文，然后保留英语版本备用。\n" +
+                    "\"\"\"\n" +
+                    "为了让你更好的理解提示词，我收集了一些表现效果较好的提示词案例，你可以对照上面的规则学习吸收：\n" +
+                    "\"\"\"\n" +
+                    "Maximalist chaotic buenos aires, birds eye view, illustrated by hergé, style of tin tin comics, pen and ink\n" +
+                    "\n" +
+                    "Hyper -realistic girl in blue coat, left in the woods, weird, otherworldly, real, vintage photograph, film set, 85mm lens, f/2.8 aperture\n" +
+                    "\n" +
+                    "instagram photo of a Japanese and Scandinavian design style living room with lots of golden light, hyperrealistic surrealism, award winning masterpiece with incredible details\n" +
+                    "\n" +
+                    "Portrait of an owl, steampunk, indigo blue, colorful, illustration, highly detailed, simple, smooth, and clean vector, no jagged lines, vector art, smooth, made all with grey colored gears inspired by future technology\n" +
+                    "\"\"\"\n" +
+                    "现在请根据我下面输入的内容生成提示词。" +
+                    "请不要输出除了提示词外的任何内容。"
+            },
+            {
+                role: "user",
+                content: prompt,
+            },
+        ];
+        const response = await env.AI.run("@cf/qwen/qwen1.5-14b-chat-awq", {messages});
+        const imgPrompt = response.response
+        console.log("图片提示词：",imgPrompt);
         const inputs = {
-            prompt: searchParams.get('prompt')
+            prompt: imgPrompt
         };
-        const response = await env.AI.run(
+        const imgResponse = await env.AI.run(
             "@cf/bytedance/stable-diffusion-xl-lightning",
             inputs
         );
-        return new Response(response, {
+        return new Response(imgResponse, {
             headers: {
                 "content-type": "image/png"
             }
